cb0e1e62afc4a2e4ec2c1b69c8fef9f5ffbe7a59
davidchuang5 <davidchuang5@gmail.com>
2024-02-27 12:42:20 -0600

    First commit

---
+++ components/Searchbar.tsx
@@ -0,0 +1,12 @@
+const Searchbar = () => {
+  return (
+    <form className="flex flex-wrap gap-4 mt-12">
+      <input type="text" placeholder="Enter product link" className="searchbar-input" />
+      <button type="submit" className="searchbar-btn">
+        Search
+      </button>
+    </form>
+  );
+};
+
+export default Searchbar;

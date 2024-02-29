9388d85078519661b68103c876533471358b8ee4
davidchuang5 <davidchuang5@gmail.com>
2024-02-29 13:05:29 -0600

    Fixed package-lock.json file which was causing major issues. Reverted changes back to working commit. Homepage currently has read/write access to database and price extractions are working.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image';
 import Searchbar from '@/components/Searchbar';
 import HeroCarousel from '@/components/HeroCarousel';
+import { getAllProducts } from '@/lib/actions';
+import ProductCard from '@/components/ProductCard';
 
-const Home = () => {
+const Home = async () => {
+  const allProducts = await getAllProducts();
   return (
     <>
       <section className="px-6 md:px-20 py-24 ">
@@ -26,6 +29,16 @@ const Home = () => {
           </div>
 
           <HeroCarousel />
+        </div>
+      </section>
+
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
         </div>
       </section>
     </>

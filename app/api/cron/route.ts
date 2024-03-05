961f99bd49db4d41f8af53aad93e3336f095e24f
davidchuang5 <davidchuang5@gmail.com>
2024-03-05 15:57:23 -0600

    Added await to mongo connection

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -12,7 +12,7 @@ export const revalidate = 0;
 
 export async function GET(request: Request) {
   try {
-    connectToDB();
+    await connectToDB();
 
     const products = await Product.find({}).maxTimeMS(30000).exec();
 

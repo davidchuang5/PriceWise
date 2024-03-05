c5d30db3dee418c02e0d77cfaf7b90c91a3f85c7
davidchuang5 <davidchuang5@gmail.com>
2024-03-05 12:15:34 -0600

    Change extractDescription function

--- lib/utils.ts
+++ lib/utils.ts
@@ -46,13 +46,11 @@ export function extractCurrency(element: any) {
 // Extracts description from two possible elements from amazon
 export function extractDescription($: any) {
   // these are possible elements holding description of the product
-  const selectors = [
-    '#productDescription',
-    // Add more selectors here if needed
-  ];
+  const selectors = ['.a-unordered-list .a-list-item', '.a-expander-content p'];
 
   for (const selector of selectors) {
     const elements = $(selector);
+
     if (elements.length > 0) {
       const textContent = elements
         .map((_: any, element: any) => $(element).text().trim())

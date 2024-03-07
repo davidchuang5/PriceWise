25696c99668e495cd027119780235b5a834a1cac
davidchuang5 <davidchuang5@gmail.com>
2024-03-07 12:50:13 -0600

    Changed order of mongo connection thread to fix timeout error

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -3,20 +3,20 @@ import mongoose from 'mongoose';
 let isConnected = false; // Variable to track the connection status
 
 export const connectToDB = async () => {
-  mongoose.set('strictQuery', true);
+  //mongoose.set('strictQuery', true);
 
   if (!process.env.MONGODB_URI) return console.log('MONGODB_URI is not defined');
 
   if (isConnected) return console.log('=> using existing database connection');
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
 
     isConnected = true;
 
-    console.log('MongoDB Connected');
-    return true;
+    console.log(`MongoDB Connected, ${conn.connection.host}`);
+    return conn;
   } catch (error) {
-    console.log(error);
+    console.error('Connection failed', error);
   }
 };
